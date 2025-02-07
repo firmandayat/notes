526aba2659cb923d8452c17db3b3d4b710cd2334
firmandayat <firman.txt@gmail.com>
2025-02-08 01:25:07 +0700

    add delete confirm

--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -4,17 +4,36 @@ import { FaTrash } from 'react-icons/fa';
 import { LocaleConsumer } from '../contexts/LocaleContext';
 
 function DeleteButton({ id, onDelete }) {
+  const handleDelete = (locale) => {
+    const confirmationMessage =
+      locale === 'en'
+        ? 'Are you sure you want to delete this note?'
+        : 'Apakah Anda yakin ingin menghapus catatan ini?';
+
+    const confirmation = window.confirm(confirmationMessage);
+    if (confirmation) {
+      onDelete(id);
+    }
+  };
+
   return (
-    <button className="note-item__delete-button" onClick={() => onDelete(id)}>
-      <FaTrash />
-      <LocaleConsumer>{({ locale }) => (<span>{locale === 'en' ? ' Delete' : ' Hapus'}</span>)}</LocaleConsumer>
-    </button>
+    <LocaleConsumer>
+      {({ locale }) => (
+        <button
+          className="note-item__delete-button"
+          onClick={() => handleDelete(locale)}
+        >
+          <FaTrash />
+          <span>{locale === 'en' ? ' Delete' : ' Hapus'}</span>
+        </button>
+      )}
+    </LocaleConsumer>
   );
 }
 
 DeleteButton.propTypes = {
   id: PropTypes.string,
-  onDelete: PropTypes.func
+  onDelete: PropTypes.func,
 };
 
 export default DeleteButton;
